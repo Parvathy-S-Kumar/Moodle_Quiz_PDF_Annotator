117b21cc485a1b1c69112182c01552857d283c39
Parvathy-S-Kumar <72549252+Parvathy-S-Kumar@users.noreply.github.com>
2023-05-09 10:55:53 +0530

    Defined constants

--- 4.0/mod/quiz/parsefunctions.php
+++ 4.0/mod/quiz/parsefunctions.php
@@ -7,12 +7,20 @@
  * Depending on the data, different objects can be drawn on top of a pdf using these functions.
  */
 
+
+define("BRUSHSIZE",0.60);
+define("FONTTYPE",'Times');
+define("OPACITY",0.20);
+define("FULLOPACITY",1);
+define("FONTRATIO",1.6);
+
 // Function to draw free hand drawing
 function draw_path($arr, $pdf) 
 {
     $list = parser_path($arr);
     $stroke = process_color(end($list));
     $pdf->SetDrawColor($stroke[0], $stroke[1], $stroke[2]);   // r g b of stroke color
+    $pdf->SetLineWidth(BRUSHSIZE);
     for($k = 0; $k < sizeof($list) - 2; $k++) {
         $pdf->Line($list[$k][0],                      // x1
         $list[$k][1],                                 // y1
@@ -27,11 +35,11 @@ function insert_text($arr,$pdf)
     $list = parser_text($arr);
     $color = process_color($list[5]);
     $pdf->SetTextColor($color[0], $color[1], $color[2]);       // r g b
-    $pdf->SetFont('Times');                                    // times roman font
+    $pdf->SetFont(FONTTYPE);                                  
     // converting fabricjs font size to that of fpdf
-    $pdf->SetFontSize(($list[6]/1.6));
+    $pdf->SetFontSize($list[6]/FONTRATIO);                                         
     $pdf->text($list[0],                                       // x
-    $list[1] + $list[3],                                       // y  (y base + height)
+    $list[1] + $list[3],                                       // y  ( base + height)
     $list[4]);                                                 // text content
 }
 
@@ -41,12 +49,12 @@ function draw_rect($arr,$pdf)
     $list = parser_rectangle($arr);
     $fill = process_color($list[4]);
     $pdf->SetFillColor($fill[0],$fill[1],$fill[2]); // r g b
-    $pdf->SetAlpha(0.20);                     // for highlighting
+    $pdf->SetAlpha(OPACITY);                     // for highlighting
     $pdf->Rect($list[0],                      // x
     $list[1],                                 // y
     $list[2],                                 // width
     $list[3],'F');                            // height
     // F refers to syle fill
-    $pdf->SetAlpha(1);
+    $pdf->SetAlpha(FULLOPACITY);
 }
 ?>

371180c434c099d26f2b5753cfcab9ec43d0c246
Parvathy-S-Kumar <72549252+Parvathy-S-Kumar@users.noreply.github.com>
2023-05-17 12:20:23 +0530

    Update renderer.php 4.2 comments

--- src/4.2/question/type/essay/renderer.php
+++ src/4.2/question/type/essay/renderer.php
@@ -21,6 +21,9 @@
  * @subpackage essay
  * @copyright  2009 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * Changes were made by Tausif Iqbal and Vishal Rao on line numbers
+ * 81-82, 90-91, 109-115, 123-193
+ * No changes made by Asha Jose and Parvathy S Kumar
  */
 
 
